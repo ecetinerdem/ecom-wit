542cb580e062bd212ee140461b692dc9d909f630
Cetin Erdem <cetoerdem1@gmail.com>
2024-10-27 11:53:34 +0400

    add product logic

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import products from '../data/productsData';
+
 import Header from '@/layouts/Header';
 import Brands from '@/components/Brands';
 import Footer from '@/layouts/Footer';
@@ -8,13 +8,6 @@ import ProductPageContent from '@/layouts/ProductPageContent';
 
 
 const ProductDetailPage = () => {
-  const { id } = useParams(); // Get the product ID from the URL
-  const product = products.find((p) => p.id === parseInt(id)); // Find the matching product
-
-  if (!product) {
-    return <p>Product not found!</p>;
-  }
-
   return (
     <div>
       
